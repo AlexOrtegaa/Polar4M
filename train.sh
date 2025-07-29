04c706e914a4b61892894dd20b19a47767861d56
AlexOrtegaa <alexortegacp@gmail.com>
2025-07-29 17:03:10 -0400

    last changes

--- train.sh
+++ train.sh
@@ -29,3 +29,19 @@ srun $PYTHON -m scripts.run_training -c $CONFIG
 
 # copy results to dropbox (run externally)
 # rsync -avz -e 'ssh -A -J walml@external.jb.man.ac.uk' walml@galahad.ast.man.ac.uk:"/share/nas2/walml/repos/foundationalmodel-possum/checkpoints/*/epoch_1*" .
+
+
+#!/bin/bash
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-03:00
+#SBATCH --output=notifications.out
+
+module load python/3.11.5 # Make sure to choose a version that suits your application
+module load scipy-stack/2025a
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index -r requirements.txt
+
+python -u -m scripts.run_training -c config_peak_polarisation_gpu
